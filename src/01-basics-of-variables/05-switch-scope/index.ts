dea62186ff091b431570506959787c0b63015769
squidmin <morse.james.r@gmail.com>
2023-09-12 22:30:52 -0500

    edit: remove comment

--- src/01-basics-of-variables/05-switch-scope/index.ts
+++ src/01-basics-of-variables/05-switch-scope/index.ts
@@ -1,26 +1,3 @@
-/*
- * The switch statement requires curly brackets after the colon and after the
- *   break statement. Otherwise, variables defined within the parent scope are
- *   shared. This is not a constraint with TypeScript, but it is with ECMAScript.
- */
-
-// Doesn't compile
-// function switchFunction(a: number): void {
-//   switch (a) {
-//     case 1:
-//       let variableInCase1 = 'test';
-//       console.log(variableInCase1);
-//       break;
-//     case 2:
-//       let variableInCase1 = 'test2';
-//       console.log(variableInCase1);
-//       break;
-//     default:
-//       console.log('Default');
-//   }
-// }
-
-// Compiles
 function switchFunction(a: number): void {
   switch (a) {
     case 1: {
